4e8c9695c8f795b622bf423169d8a78a591ddb36
Jason Stephens <jassteph@redhat.com>
2020-01-14 16:05:29 -0500

    Adding a new test

--- internal/controller/ws_controller_test.go
+++ internal/controller/ws_controller_test.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/RedHatInsights/platform-receptor-controller/internal/receptor/protocol"
@@ -9,12 +11,31 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	//c "github.com/RedHatInsights/platform-receptor-controller/internal/controller"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-	. "github.com/gorilla/websocket"
 	"github.com/posener/wstest"
 )
+
+func readSocket(c *websocket.Conn, mt protocol.NetworkMessageType) protocol.Message {
+	mtype, r, _ := c.NextReader()
+	Expect(mtype).Should(Equal(websocket.BinaryMessage))
+
+	fmt.Println("TestClient reading response from receptor-controller")
+	m, _ := protocol.ReadMessage(r)
+	Expect(m.Type()).Should(Equal(mt))
+
+	return m
+}
+
+func writeSocket(c *websocket.Conn, message protocol.Message) {
+	w, err := c.NextWriter(websocket.BinaryMessage)
+	Expect(err).NotTo(HaveOccurred())
+
+	fmt.Println("TestClient writing to receptor-controller")
+	err = protocol.WriteMessage(w, message)
+	Expect(err).NotTo(HaveOccurred())
+	w.Close()
+}
 
 var _ = Describe("WsController", func() {
 	var (
@@ -22,7 +43,7 @@ var _ = Describe("WsController", func() {
 		wsMux    *mux.Router
 		cm       *ConnectionManager
 		rc       *ReceptorController
-		d        *Dialer
+		d        *websocket.Dialer
 		header   http.Header
 	)
 
@@ -53,10 +74,19 @@ var _ = Describe("WsController", func() {
 				Expect(err).To(HaveOccurred())
 			})
 		})
-		Context("With a malformed identity header", func() {
+		Context("With an empty identity header", func() {
 			It("Should not be able to open a connection", func() {
 				id := header["x-rh-identity"]
 				id[0] = ""
+				header["x-rh-identity"] = id
+				_, _, err := d.Dial("ws://localhost:8080/receptor-controller", header)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+		Context("With a malformed identity header", func() {
+			It("Should not be able to open a connection", func() {
+				id := header["x-rh-identity"]
+				id[0] = `{ "account_number": "540155 }`
 				header["x-rh-identity"] = id
 				_, _, err := d.Dial("ws://localhost:8080/receptor-controller", header)
 				Expect(err).To(HaveOccurred())
@@ -67,23 +97,48 @@ var _ = Describe("WsController", func() {
 	Describe("Connecting to the receptor controller with a handshake", func() {
 		Context("With an open connection and sending Hi", func() {
 			It("Should in return receive a HiMessage defined in protocol pkg", func() {
-				hiMessage := protocol.HiMessage{Command: "HI", ID: "test-node"}
 				c, _, err := d.Dial("ws://localhost:8080/receptor-controller", header)
 				Expect(err).NotTo(HaveOccurred())
 				defer c.Close()
 
-				w, err := c.NextWriter(websocket.BinaryMessage)
-				err = protocol.WriteMessage(w, &hiMessage)
-				Expect(err).NotTo(HaveOccurred())
-				w.Close()
+				hiMessage := protocol.HiMessage{Command: "HI", ID: "TestClient"}
+				writeSocket(c, &hiMessage)
 
-				mtype, r, err := c.NextReader()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(mtype).Should(Equal(websocket.BinaryMessage))
+				m := readSocket(c, 1)
+				Expect(m.Type()).To(Equal(protocol.HiMessageType))
+			})
+		})
+	})
 
-				m, err := protocol.ReadMessage(r)
+	Describe("Connecting to the receptor controller and sending work", func() {
+		Context("With an open connection", func() {
+			It("Should send a ping directive", func() {
+				c, _, err := d.Dial("ws://localhost:8080/receptor-controller", header)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(m.Type()).Should(Equal(protocol.HiMessageType))
+				defer c.Close()
+
+				hiMessage := protocol.HiMessage{Command: "HI", ID: "TestClient"}
+				writeSocket(c, &hiMessage)
+
+				_ = readSocket(c, 1)
+
+				client := rc.connectionMgr.GetConnection("01")
+
+				workRequest := Work{MessageID: "123",
+					Recipient: "TestClient",
+					RouteList: []string{"test-b", "test-a"},
+					Payload:   "hello",
+					Directive: "receptor:ping"}
+
+				client.SendWork(workRequest)
+
+				m := readSocket(c, 4)      // read response from SendWork request and verify it is a PayloadMessage
+				jm, err := json.Marshal(m) // m's marshal/unmarshal functions are private and can't be used here
+
+				// FIXME: Need a better way to verify the message
+				Expect(string(jm)).To(ContainSubstring("\"sender\":\"node-cloud-receptor-controller\""))
+				Expect(string(jm)).To(ContainSubstring("\"recipient\":\"TestClient\""))
+				Expect(string(jm)).To(ContainSubstring("\"directive\":\"receptor:ping\""))
 			})
 		})
 	})
