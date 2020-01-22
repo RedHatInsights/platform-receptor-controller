eac265b4b6ff1a3a56679be394b608bc5a144967
Jason Stephens <jassteph@redhat.com>
2020-01-22 17:20:27 -0500

    Fixing race condition. Will need to update with new context changes

--- internal/controller/ws_controller_test.go
+++ internal/controller/ws_controller_test.go
@@ -38,6 +38,15 @@ func writeSocket(c *websocket.Conn, message protocol.Message) {
 	w.Close()
 }
 
+func getClient(rc *ReceptorController, accountID string, nodeID string) Client {
+	client := rc.connectionMgr.GetConnection(accountID, nodeID)
+	if client == nil {
+		fmt.Println("Client not registered yet.. retrying")
+		getClient(rc, accountID, nodeID)
+	}
+	return client
+}
+
 func leaks() error {
 	return goleak.Find(goleak.IgnoreTopFunction("github.com/onsi/ginkgo/internal/specrunner.(*SpecRunner).registerForInterrupts"))
 }
@@ -123,9 +132,6 @@ var _ = Describe("WsController", func() {
 	Describe("Connecting to the receptor controller and sending work", func() {
 		Context("With an open connection", func() {
 			It("Should send a ping directive", func() {
-
-				Skip("Skipping for now.  This test is broken due to a race condition.")
-
 				c, _, err := d.Dial("ws://localhost:8080/wss/receptor-controller/gateway", header)
 				Expect(err).NotTo(HaveOccurred())
 				defer c.Close()
@@ -137,12 +143,7 @@ var _ = Describe("WsController", func() {
 
 				_ = readSocket(c, 1)
 
-				// FIXME: The test fails here.  The issue is that the go routine that handles the
-				// web socket performs the handshake...which sends a HiMessage back to the client
-				// BEFORE the connection is registered with the connection manager.  This means that
-				// if the timing is right and this client code runs as a different go routine, then
-				// client is nil below.
-				client := rc.connectionMgr.GetConnection("540155", nodeID)
+				client := getClient(rc, "540155", nodeID)
 
 				workRequest := Work{MessageID: "123",
 					Recipient: "TestClient",
