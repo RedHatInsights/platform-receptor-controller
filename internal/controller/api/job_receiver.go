3606461a927da8eb306ec1b3510a41b79445ee56
Derek Horton <dehort@redhat.com>
2020-03-04 17:31:13 -0600

    Removed panics

--- internal/controller/api/job_receiver.go
+++ internal/controller/api/job_receiver.go
@@ -56,20 +56,22 @@ func (jr *JobReceiver) handleJob() http.HandlerFunc {
 		var jobRequest JobRequest
 
 		body, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
-
 		if err != nil {
-			panic(err)
+			http.Error(w, err.Error(), 500)
+			return
 		}
 
 		if err := req.Body.Close(); err != nil {
-			panic(err)
+			http.Error(w, err.Error(), 500)
+			return
 		}
 
 		if err := json.Unmarshal(body, &jobRequest); err != nil {
 			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 			w.WriteHeader(http.StatusUnprocessableEntity)
 			if err := json.NewEncoder(w).Encode(err); err != nil {
-				panic(err)
+				http.Error(w, err.Error(), 500)
+				return
 			}
 		}
 
@@ -98,7 +100,9 @@ func (jr *JobReceiver) handleJob() http.HandlerFunc {
 			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 			w.WriteHeader(http.StatusUnprocessableEntity)
 			if err := json.NewEncoder(w).Encode(err); err != nil {
-				panic(err)
+				http.Error(w, err.Error(), 500)
+				return
+
 			}
 		}
 
@@ -107,7 +111,8 @@ func (jr *JobReceiver) handleJob() http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusCreated)
 		if err := json.NewEncoder(w).Encode(jobResponse); err != nil {
-			panic(err)
+			http.Error(w, err.Error(), 500)
+			return
 		}
 	}
 }
