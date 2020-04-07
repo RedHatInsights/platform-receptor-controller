06985115816fc4d1b457249f1af019323d6f1036
Jason Stephens <jassteph@redhat.com>
2020-04-07 17:24:29 -0400

    Forgot to push new files...

---
+++ internal/platform/queue/types.go
@@ -0,0 +1,13 @@
+package queue
+
+// type ProducerConfig struct {
+// 	Brokers []string
+// 	Topic   string
+// }
+
+// type ConsumerConfig struct {
+// 	Brokers        []string
+// 	Topic          string
+// 	GroupID        string
+// 	ConsumerOffset int64
+// }
