4e19a6fff4fc7f43b8e973cb289247645f5d2a04
Derek Horton <dehort@redhat.com>
2020-04-22 15:41:58 -0500

    Add explicit checks for the account number at the websocket layer

--- internal/controller/transport.go
+++ internal/controller/transport.go
@@ -6,10 +6,20 @@ import (
 	"github.com/RedHatInsights/platform-receptor-controller/internal/receptor/protocol"
 )
 
+type SendMessage struct {
+	AccountNumber string
+	Message       protocol.Message
+}
+
+type SendErrorMessage struct {
+	AccountNumber string
+	Error         error
+}
+
 type Transport struct {
 
 	// send is a channel on which messages are sent.
-	Send chan protocol.Message
+	Send chan SendMessage
 
 	// recv is a channel on which responses are sent.
 	Recv chan protocol.Message
@@ -18,11 +28,11 @@ type Transport struct {
 	// messages are passed to the write side of the websocket...
 	// this allows command/control messages to bypass any queued
 	// job messages
-	ControlChannel chan protocol.Message
+	ControlChannel chan SendMessage
 
 	// errorChannel is a channel on which errors are sent
 	// to the go routine managing write side of the websocket
-	ErrorChannel chan error
+	ErrorChannel chan SendErrorMessage
 
 	Ctx    context.Context
 	Cancel context.CancelFunc
