f21d9885c5813d8bf6e46f0716cf1f051ea34b5f
Derek Horton <dehort@redhat.com>
2020-06-05 11:16:45 -0500

    Adding a simple kafka reader util to the deployed image to help troubleshoot kafka message dispatching

---
+++ cmd/response_consumer/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/RedHatInsights/platform-receptor-controller/internal/config"
+
+	"github.com/google/uuid"
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+	cfg := config.GetConfig()
+
+	groupID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println("Unable to generate random uuid:", err)
+		return
+	}
+
+	kafkaConfig := kafka.ReaderConfig{
+		Brokers:     cfg.KafkaBrokers,
+		Topic:       cfg.KafkaResponsesTopic,
+		GroupID:     groupID.String(),
+		StartOffset: cfg.KafkaConsumerOffset,
+	}
+
+	kafkaReader := kafka.NewReader(kafkaConfig)
+
+	fmt.Println("Kafka consumer config: ", kafkaReader.Config())
+
+	for {
+		m, err := kafkaReader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("Got an error:", err)
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	kafkaReader.Close()
+}
