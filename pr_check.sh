5f3803542102329a3b3555b1f8cfba08fed05534
PJ Richardson <52798694+prichard77@users.noreply.github.com>
2021-08-24 12:18:51 -0400

    Update pr_check.sh
    Finished up configuring this for receptor

--- pr_check.sh
+++ pr_check.sh
@@ -3,13 +3,14 @@
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="ingress"  # name of app-sre "application" folder this component lives in
+APP_NAME="receptor"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="receptor-gateway"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/receptor-gateway"  
+IMAGE="quay.io/cloudservices/receptor-gateway"
 
-IQE_PLUGINS="ingress"
+IQE_PLUGINS="controller_interface"
 IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
+IQE_CJI_TIMEOUT="30m"
 
 
 # Install bonfire repo/initialize
@@ -18,4 +19,5 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
-source $CICD_ROOT/smoke_test.sh
+source $CICD_ROOT/cji_smoke_test.sh
+
