4b4b511b17e5094fac6be6cbbeeb26c8bfca4980
Derek Horton <dehort@redhat.com>
2022-01-07 14:07:04 -0600

    Testing...

--- pr_check.sh
+++ pr_check.sh
@@ -19,5 +19,6 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
+COMPONENT_NAME="receptor"
 source $CICD_ROOT/cji_smoke_test.sh
 
