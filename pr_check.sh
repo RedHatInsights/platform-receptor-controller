f7ae7892d8e39cb50a2325f27923840a226fc9d0
dehort <dehort@redhat.com>
2022-02-03 10:38:22 -0600

    Rebuild the deployment images due to CVEs (#194)
    * Rebuild the deployment images due to CVEs
    
    * Try to fix the pr_check (again)

--- pr_check.sh
+++ pr_check.sh
@@ -18,7 +18,7 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
-source $CICD_ROOT/deploy_ephemeral_env.sh
 COMPONENT_NAME="receptor"
+source $CICD_ROOT/deploy_ephemeral_env.sh
 source $CICD_ROOT/cji_smoke_test.sh
 
