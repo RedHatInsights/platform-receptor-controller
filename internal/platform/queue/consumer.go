434acea307c437895cf7c4e7c67eb4277d9f5e82
Stephen Adams <stephen.adams@redhat.com>
2022-06-28 13:57:25 -0400

    add consumer and producer sasl support
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- internal/platform/queue/consumer.go
+++ internal/platform/queue/consumer.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"github.com/RedHatInsights/platform-receptor-controller/internal/config"
 	"github.com/RedHatInsights/platform-receptor-controller/internal/platform/logger"
 	kafka "github.com/segmentio/kafka-go"
 )
@@ -9,12 +10,37 @@ func StartConsumer(cfg *ConsumerConfig) *kafka.Reader {
 	logger.Log.Info("Starting a new kafka consumer...")
 	logger.Log.Info("Kafka consumer configuration: ", cfg)
 
-	r := kafka.NewReader(kafka.ReaderConfig{
+	var kafkaDialer *kafka.Dialer
+	var err error
+
+	globalConfig := config.GetConfig()
+
+	if globalConfig.KafkaSaslUsername != "" {
+
+		kafkaDialer, err = saslDialer(&SaslConfig{
+			SaslMechanism: globalConfig.KafkaSaslMechanism,
+			SaslUsername:  globalConfig.KafkaSaslUsername,
+			SaslPassword:  globalConfig.KafkaSaslPassword,
+			KafkaCA:       globalConfig.KafkaCAPath,
+		})
+		if err != nil {
+			logger.Log.Error("Failed to create a new Kafka Dialer: ", err)
+			panic(err)
+		}
+	}
+
+	readerConfig := kafka.ReaderConfig{
 		Brokers:     cfg.Brokers,
 		Topic:       cfg.Topic,
 		GroupID:     cfg.GroupID,
 		StartOffset: cfg.ConsumerOffset,
-	})
+	}
+
+	if kafkaDialer != nil {
+		readerConfig.Dialer = kafkaDialer
+	}
+
+	r := kafka.NewReader(readerConfig)
 
 	logger.Log.Info("Kafka consumer config: ", r.Config())
 
--- internal/platform/queue/producer.go
+++ internal/platform/queue/producer.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"github.com/RedHatInsights/platform-receptor-controller/internal/config"
 	"github.com/RedHatInsights/platform-receptor-controller/internal/platform/logger"
 	kafka "github.com/segmentio/kafka-go"
 )
@@ -9,12 +10,37 @@ func StartProducer(cfg *ProducerConfig) *kafka.Writer {
 	logger.Log.Info("Starting a new Kafka producer..")
 	logger.Log.Info("Kafka producer configuration: ", cfg)
 
-	w := kafka.NewWriter(kafka.WriterConfig{
+	var kafkaDialer *kafka.Dialer
+	var err error
+
+	globalConfig := config.GetConfig()
+
+	if globalConfig.KafkaSaslUsername != "" {
+
+		kafkaDialer, err = saslDialer(&SaslConfig{
+			SaslMechanism: globalConfig.KafkaSaslMechanism,
+			SaslUsername:  globalConfig.KafkaSaslUsername,
+			SaslPassword:  globalConfig.KafkaSaslPassword,
+			KafkaCA:       globalConfig.KafkaCAPath,
+		})
+		if err != nil {
+			logger.Log.Error("Failed to create a new Kafka Dialer: ", err)
+			panic(err)
+		}
+	}
+
+	writerConfig := kafka.WriterConfig{
 		Brokers:    cfg.Brokers,
 		Topic:      cfg.Topic,
 		BatchSize:  cfg.BatchSize,
 		BatchBytes: cfg.BatchBytes,
-	})
+	}
+
+	if kafkaDialer != nil {
+		writerConfig.Dialer = kafkaDialer
+	}
+
+	w := kafka.NewWriter(writerConfig)
 
 	logger.Log.Info("Producing messages to topic: ", cfg.Topic)
 
