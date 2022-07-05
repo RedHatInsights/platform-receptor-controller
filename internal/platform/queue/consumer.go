bd305b2e7853933d23192423f6b3d0db88236b24
Derek Horton <dehort@redhat.com>
2022-07-05 09:16:36 -0500

    backport sasl changes from cloud-connector

--- internal/platform/queue/consumer.go
+++ internal/platform/queue/consumer.go
@@ -1,32 +1,19 @@
 package queue
 
 import (
-	"github.com/RedHatInsights/platform-receptor-controller/internal/config"
+	"fmt"
+
 	"github.com/RedHatInsights/platform-receptor-controller/internal/platform/logger"
 	kafka "github.com/segmentio/kafka-go"
 )
 
-func StartConsumer(cfg *ConsumerConfig) *kafka.Reader {
+func StartConsumer(cfg *ConsumerConfig) (*kafka.Reader, error) {
 	logger.Log.Info("Starting a new kafka consumer...")
 	logger.Log.Info("Kafka consumer configuration: ", cfg)
 
-	var kafkaDialer *kafka.Dialer
-	var err error
-
-	globalConfig := config.GetConfig()
-
-	if globalConfig.KafkaSaslUsername != "" {
-
-		kafkaDialer, err = saslDialer(&SaslConfig{
-			SaslMechanism: globalConfig.KafkaSaslMechanism,
-			SaslUsername:  globalConfig.KafkaSaslUsername,
-			SaslPassword:  globalConfig.KafkaSaslPassword,
-			KafkaCA:       globalConfig.KafkaCAPath,
-		})
-		if err != nil {
-			logger.Log.Error("Failed to create a new Kafka Dialer: ", err)
-			panic(err)
-		}
+	kafkaDialer, err := createDialer(cfg.SaslConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create kafka dialer: %w", err)
 	}
 
 	readerConfig := kafka.ReaderConfig{
@@ -34,15 +21,10 @@ func StartConsumer(cfg *ConsumerConfig) *kafka.Reader {
 		Topic:       cfg.Topic,
 		GroupID:     cfg.GroupID,
 		StartOffset: cfg.ConsumerOffset,
-	}
-
-	if kafkaDialer != nil {
-		readerConfig.Dialer = kafkaDialer
+		Dialer:      kafkaDialer,
 	}
 
 	r := kafka.NewReader(readerConfig)
 
-	logger.Log.Info("Kafka consumer config: ", r.Config())
-
-	return r
+	return r, nil
 }
